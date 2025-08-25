56d37804c05f214ae0f70c4019137c59c7fa4554
Poojan2808 <dobariyapoojan22@gmail.com>
2025-08-25 21:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.36%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int cal(vector<int>&arr , int a){
+        int day = 1;
+        int sum = 0;
+
+        for(int i=0;i<arr.size();i++){
+            if(sum + arr[i] <= a){
+                sum += arr[i];
+            }
+            else{
+                sum = arr[i];
+                day++;
+            }
+        }
+        return day;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int max = 0;
+        int sum =0 ;
+        for(int i=0; i<nums.size();i++){
+            if(nums[i]>max) max = nums[i];
+            sum+= nums[i];
+        }
+
+        int low = max;
+        int high = sum;
+
+        while(low <= high){
+            int mid = (low + high) /2;
+            if(cal(nums,mid) > k){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
